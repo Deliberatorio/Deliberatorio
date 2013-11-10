b348540038b86996d1648769e49456500a28f341
Valessio Brito <contato@valessiobrito.com.br>
2013-11-09 23:41:52 -0200

    Movendo o deliberation_ng para deliberation.sh

--- deliberatorio.sh
+++ deliberatorio.sh
@@ -24,124 +24,169 @@
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #   GNU General Public License for more details.
 
-#   Version 0.1
+#   Version NG 0.1 (Versão Nova Geração em Desenvolvimento)
 
-URL_ORGAOS="http://www.camara.gov.br/SitCamaraWS/Orgaos.asmx/ObterOrgaos"
 CSV_ORGAOS="$PWD/all-org.csv"
-CSV_CARDORG="$PWD/org.csv"
-TMP_ORGAOS=$( mktemp )
-
 CSV_PAUTAS="$PWD/prop.csv"
-TMP_PAUTAS=$( mktemp )
-
-URL_DEPUTADOS="http://www.camara.gov.br/SitCamaraWS/Deputados.asmx/ObterDeputados"
 CSV_DEPUTADOS="$PWD/all-dep.csv"
 CSV_CARDDEP="$PWD/dep.csv"
-TMP_DEPUTADOS=$( mktemp )
-TMP_DETALHES=$( mktemp )
+CSV_CARDORG="$PWD/org.csv"
 
-# Limpa dados anteriores
-rm -i $CSV_ORGAOS $CSV_PAUTAS $CSV_DEPUTADOS $CSV_CARDORG $CSV_CARDDEP
+TMP_PAUTAS=$( mktemp )
 
-# OBTER ORGAOS
-wget $URL_ORGAOS -O $TMP_ORGAOS
+TMP_DEPUTADOS="data/ObterDeputados.xml"
+TMP_ORGAOS="data/ObterOrgaos.xml"
+
+
+# Barra de Progresso
 COUNT=0
-TOTAL=`cat $TMP_ORGAOS | wc -l`
-ProgBar="[=======================================================================]"
 
-echo "
+function progresso {
+   TOTAL=$1
+   ProgBar="[=======================================================================]"
+   ProgCount=$(( $COUNT * 73 / $TOTAL ))
+   printf "\r%3d.%1d%% %.${ProgCount}s" $(( $COUNT * 100 / $TOTAL )) $(( ($COUNT * 1000 / $TOTAL) % 10 )) $ProgBar
+}
 
-    Obtendo Pauta da Semana:"
+# Questiona ao usuário se deve gerar novos dados
+echo "Gerar nova base de Órgãos? [S]im ou [N]ão"
+rm -i $CSV_ORGAOS 2> /dev/null
+if [ -f $CSV_ORGAOS ]
+then
+   echo "Mantido a relação de Orgãos"
+else
+    echo "
 
-for idOrgao in `xmlstarlet sel -t -v "//orgaos/orgao/@id" $TMP_ORGAOS` ; do
-    let COUNT++
+   Obtendo Órgãos:"
+   for idOrgao in `xmlstarlet sel -t -v "//orgaos/orgao/@id" $TMP_ORGAOS` ; do
+      let COUNT++
 
-    # Parse no XML da sigla e descrição do Orgão
-    siglaOrgao=$(xmlstarlet sel -t -v "//orgaos/orgao[$COUNT]/@sigla" $TMP_ORGAOS)
-    descricaoOrgao=$(xmlstarlet sel -t -v "//orgaos/orgao[$COUNT]/@descricao" $TMP_ORGAOS)
+      progresso $(xmlstarlet sel -t -v "//orgaos/orgao/@id" $TMP_ORGAOS | wc -l)
 
-    # Plotando no CSV dos Orgãos
-    echo $idOrgao\;$siglaOrgao\;$descricaoOrgao >> $CSV_ORGAOS
+      # Parse no XML da sigla e descrição do Orgão
+      siglaOrgao=$(xmlstarlet sel -t -v "//orgaos/orgao[$COUNT]/@sigla" $TMP_ORGAOS)
+      descricaoOrgao=$(xmlstarlet sel -t -v "//orgaos/orgao[$COUNT]/@descricao" $TMP_ORGAOS)
 
-    # OBTER PAUTAS
-    URL_PAUTAS="http://www.camara.gov.br/SitCamaraWS/Orgaos.asmx/ObterPauta?IDOrgao=$idOrgao&datIni=$(date +%d\/%m\/%Y)&datFim=$(date +%d\/%m\/%Y -d "+10 days")"
-    wget $URL_PAUTAS -O $TMP_PAUTAS 2> /dev/null
-    PautaCOUNT=0
+      # Plotando no CSV dos Orgãos
+      echo $idOrgao\;$siglaOrgao\;$descricaoOrgao >> $CSV_ORGAOS
 
-    # Progresso da Pauta
-    ProgPauta=$(( $COUNT * 73 / $TOTAL ))
-    printf "\r%3d.%1d%% %.${ProgPauta}s" $(( $COUNT * 100 / $TOTAL )) $(( ($COUNT * 1000 / $TOTAL) % 10 )) $ProgBar
+   done
+      echo "
+      Relação de Orgãos atualizado.
+      "
+fi
 
-    # Obter detalhes da Pauta
-    for idPauta in `xmlstarlet sel -t -v "//pauta/reuniao/proposicoes/proposicao/sigla" $TMP_PAUTAS | sed s/\ /_/g` ; do
-        let PautaCOUNT++
-        ementaPauta=$(xmlstarlet sel -t -v "//pauta/reuniao/proposicoes/proposicao[$PautaCOUNT]/ementa" $TMP_PAUTAS)
-        pontoPauta=$(echo $idPauta | rev | cut -c -2 | rev)
-        nomePauta=$(echo $idPauta | sed s/"\/"/":"/g)
-        echo $pontoPauta\;$(echo $idPauta | sed s/_/\ /g)\;$siglaOrgao\;$ementaPauta\;$nomePauta \
-            | grep -E 'PL_|PEC_' \
-            | grep -vE '(Altera|nova redação|revoga|Acrescenta|REQ_)'
-    done | head -5 >> $CSV_PAUTAS
+echo "Atualizar a Pauta da Semana? [S]im ou [N]ão"
+rm -i $CSV_PAUTAS 2> /dev/null
+if [ -f $CSV_PAUTAS ]
+then
+   echo "Mantido a versão atual da Pauta."
+else
+   COUNT=0
+   echo "
 
-done # End idOrgaos
+   Obtendo Pauta da Semana:"
+   for idOrgao in `cat $CSV_ORGAOS | cut -d";" -f1`; do
+   # OBTER PAUTAS
+   URL_PAUTAS="http://www.camara.gov.br/SitCamaraWS/Orgaos.asmx/ObterPauta?IDOrgao=$idOrgao&datIni=$(date +%d\/%m\/%Y)&datFim=$(date +%d\/%m\/%Y -d "+6 days")"
+   wget $URL_PAUTAS -O $TMP_PAUTAS 2> /dev/null
+   let COUNT++
+   siglaOrgao=$(grep $idOrgao $CSV_ORGAOS | cut -d";" -f2)
+   # Progresso da Pauta
+   progresso $(cat $CSV_ORGAOS| wc -l)
 
-echo "
+   # Obter detalhes da Pauta
+   PautaCOUNT=0
+   for idPauta in `xmlstarlet sel -t -v "//pauta/reuniao/proposicoes/proposicao/sigla" $TMP_PAUTAS | sed s/\ /_/g` ; do
+      let PautaCOUNT++
+      ementaPauta=$(xmlstarlet sel -t -v "//pauta/reuniao/proposicoes/proposicao[$PautaCOUNT]/ementa" $TMP_PAUTAS)
+      pontoPauta=$(echo $idPauta | rev | cut -c -2 | rev)
+      nomePauta=$(echo $idPauta | sed s/"\/"/":"/g)
+      echo $pontoPauta\;$(echo $idPauta | sed s/_/\ /g)\;$(echo $siglaOrgao | head -1 | cut -d" " -f1)\;$ementaPauta\;$nomePauta \
+      | grep -E 'PL_|PEC_' \
+      | grep -vE '(Altera|§|nova redação|revoga|Acrescenta|REQ_)'
+   done | head -3 >> $CSV_PAUTAS
+   done #idOrgao
+   echo "
 
     Obtenção das pautas, finalizado.
 
-"
+   "
+fi
 
 # OBTER DEPUTADOS
-wget $URL_DEPUTADOS -O $TMP_DEPUTADOS
 COUNT=0
-TOTAL=`cat $TMP_DEPUTADOS | wc -l`
 
-echo "
+echo "Gerar novos dados dos Deputados? [S]im ou [N]ão"
+rm -i $CSV_DEPUTADOS 2> /dev/null
 
-    Obtendo Deputados:"
+if [ -f $CSV_DEPUTADOS ]
+then
+   echo "Dados dos Deputados mantido."
+else
+   echo "
 
-for ideCadastro in `xmlstarlet sel -t -v "//deputados/deputado/ideCadastro" $TMP_DEPUTADOS` ; do
-    let COUNT++
+   Obtendo Deputados:"
+   idDeputados=`xmlstarlet sel -t -v "//deputados/deputado/ideCadastro" $TMP_DEPUTADOS`
 
-    # Parser XML dos detalhes do Deputado
-    nomeParlamentar=$(xmlstarlet sel -t -v "//deputados/deputado[$COUNT]/nomeParlamentar" $TMP_DEPUTADOS)
-    partidoDeputado=$(xmlstarlet sel -t -v "//deputados/deputado[$COUNT]/partido" $TMP_DEPUTADOS)
-    ufDeputado=$(xmlstarlet sel -t -v "//deputados/deputado[$COUNT]/uf" $TMP_DEPUTADOS)
-    urlFoto=$(xmlstarlet sel -t -v "//deputados/deputado[$COUNT]/urlFoto" $TMP_DEPUTADOS)
-    sexoDep=$(xmlstarlet sel -t -v "/deputados/deputado[$COUNT]/sexo" $TMP_DEPUTADOS)
+   for ideCadastro in $idDeputados ; do
+      let COUNT++
 
-    # Capturando detalhe do deputado no orgão
-    URL_DETALHE="http://www.camara.gov.br/SitCamaraWS/Deputados.asmx/ObterDetalhesDeputado?ideCadastro=$ideCadastro&numLegislatura="
-    siglaDeputado=$(lynx -source "$URL_DETALHE" | xmlstarlet sel -t -v "//Deputados/Deputado/comissoes/comissao[last()]/siglaComissao" | uniq)
+      # Progresso dos Deputados
+      progresso $(echo $idDeputados | wc -l)
 
-    # Gerando CSV dos Deputados
-    echo $ideCadastro\;$nomeParlamentar\;$partidoDeputado\;$ufDeputado\;$siglaDeputado\;$urlFoto\;$sexoDep >> $CSV_DEPUTADOS
+      # Parser XML dos detalhes do Deputado
+      nomeParlamentar=$(xmlstarlet sel -t -v "//deputados/deputado[$COUNT]/nomeParlamentar" $TMP_DEPUTADOS)
+      partidoDeputado=$(xmlstarlet sel -t -v "//deputados/deputado[$COUNT]/partido" $TMP_DEPUTADOS)
+      ufDeputado=$(xmlstarlet sel -t -v "//deputados/deputado[$COUNT]/uf" $TMP_DEPUTADOS)
+      urlFoto=$(xmlstarlet sel -t -v "//deputados/deputado[$COUNT]/urlFoto" $TMP_DEPUTADOS)
+      sexoDep=$(xmlstarlet sel -t -v "/deputados/deputado[$COUNT]/sexo" $TMP_DEPUTADOS)
 
-    # Progresso dos Deputados
-    ProgDeputado=$(( $COUNT * 73 / $TOTAL ))
-    printf "\r%3d.%1d%% %.${ProgPauta}s" $(( $COUNT * 100 / $TOTAL )) $(( ($COUNT * 1000 / $TOTAL) % 10 )) $ProgBar
+      # Capturando detalhe do deputado no orgão
+      # URL_DETALHE="http://www.camara.gov.br/SitCamaraWS/Deputados.asmx/ObterDetalhesDeputado?ideCadastro=$ideCadastro&numLegislatura="
+      URL_DETALHE="data/$ideCadastro.xml"
 
-done #ideCadastro
+      siglaDeputado=$(xmlstarlet sel -t -v "//Deputados/Deputado/comissoes/comissao[last()]/siglaComissao" $URL_DETALHE | uniq)
 
-echo "
+      # Gerando CSV dos Deputados
+      echo $ideCadastro\;$nomeParlamentar\;$partidoDeputado\;$ufDeputado\;$siglaDeputado\;$urlFoto\;$sexoDep >> $CSV_DEPUTADOS
+
+   done
+
+   echo "
 
 Deputados obtidos com sucesso.
 
-"
+   "
+fi
 
-# Gera lista de Orgaos na Pauta
-listOrgPauta=`cat $CSV_PAUTAS | cut -d";" -f 3| sort | uniq`
+echo "Gerar nova base para os cartões? [S]im ou [N]ão"
+rm -i $CSV_CARDDEP 2> /dev/null
+if [ -f $CSV_CARDDEP ]
+then
+   echo "Base de cartões mantido."
+else
+   # Gera CSV de Orgaos e Deputados na Pauta
+   listOrgPauta=`cat $CSV_PAUTAS | cut -d";" -f 3| sort | uniq`
 
-echo "Gerando CSV dos cartões para o jogo."
-# Função para Gerar CSV na Pauta
-function gerar_cards {
-for cardOrgao in $listOrgPauta; do
-    grep $cardOrgao $1
-done > $2
-}
+   for cardOrgao in $listOrgPauta; do
+      grep $cardOrgao $CSV_ORGAOS
+   done | sort | uniq > $TMP_PAUTAS
+   grep -v Especial $TMP_PAUTAS > $CSV_CARDORG
 
-# Gera CSV de Orgaos e Deputados na Pauta
-gerar_cards $CSV_ORGAOS $CSV_CARDORG
-gerar_cards $CSV_DEPUTADOS $CSV_CARDDEP
+   # Filtra lista de deputados em orgãos na pauta
+   listOrgDep=`cat $CSV_CARDORG | cut -d";" -f 2| sort | uniq`
 
-echo "Todos os CSV para o jogo foram gerados, agora use o InkscapeGenerator para gerar os cartões."
+   for cardOrgao in $listOrgDep; do
+      grep $cardOrgao $CSV_DEPUTADOS | head -4
+   done  | sort | uniq > $CSV_CARDDEP
+
+   echo "Nova base de cartões gerada."
+fi
+
+echo "
+Finalizado.
+
+Gerado $(wc -l $CSV_PAUTAS | sed s/prop.csv/Proposições/g) em $(wc -l $CSV_CARDORG | sed s/org.csv/Comissões/g) com $(wc -l $CSV_CARDDEP | sed s/dep.csv/Deputados/g) envolvidos nas discussões.
+
+Agora utilize os arquivos CSV no script do generator.sh para gerar os cartões em PDF." | sed s%$PWD%%g
